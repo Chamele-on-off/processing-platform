c2bf5325f18a7f605445dc638561c13f4304604f
Chamele-on-off <mot124@yandex.ru>
2025-05-21 17:48:56 +0300

    Delete backend directory

--- backend/app/models/transactions.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float
-from app.db import Base
-
-class Transaction(Base):
-    __tablename__ = "transactions"
-    id = Column(Integer, primary_key=True)
-    amount = Column(Float)
-    status = Column(String)
-    merchant_id = Column(Integer)
--- backend/app/routers/transactions.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.models.transactions import Transaction
-
-router = APIRouter()
-
-@router.post("/transactions/")
-async def create_transaction(transaction: Transaction):
-    return {"status": "OK"}
