2fad2f5fab9f6de178e2b4b4ab01fe6c61f93c61
Chamele-on-off <mot124@yandex.ru>
2025-05-21 17:51:03 +0300

    Add files via upload

---
+++ frontend/src/components/ReceiptUpload.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="file" @change="handleUpload" accept=".pdf" />
+</template>
+
+<script setup>
+import { useTraderStore } from '@/stores/trader'
+
+const store = useTraderStore()
+
+const handleUpload = async (e) => {
+  const file = e.target.files[0]
+  await store.uploadReceipt(file)
+}
+</script>
