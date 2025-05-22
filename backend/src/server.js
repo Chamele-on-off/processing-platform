a7841a8f9d3fad0cae06976a766b2e079360c399
Chamele-on-off <mot124@yandex.ru>
2025-05-23 00:09:55 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,42 @@
 const app = require('./app');
 const config = require('./common/config');
+const db = require('./common/config/db');
+const redis = require('./common/config/redis');
+const rabbitmq = require('./common/config/rabbitmq');
+const logger = require('./common/utils/logger');
 
 const startServer = async () => {
   try {
-    // Инициализация подключений к БД и другим сервисам
-    await require('./common/config/db').connect();
-    await require('./common/config/redis').connect();
-    await require('./common/config/rabbitmq').connect();
+    // 1. Connect to DB and other services
+    await db.connect();
+    await redis.connect();
+    await rabbitmq.connect();
 
-    app.listen(config.PORT, () => {
-      console.log(`Server running on port ${config.PORT}`);
+    // 2. Start server
+    const server = app.listen(config.port, () => {
+      logger.info(`Server running on port ${config.port} in ${config.env} mode`);
+    });
+
+    // 3. Graceful shutdown
+    process.on('SIGTERM', () => {
+      logger.info('SIGTERM received. Shutting down gracefully...');
+      server.close(async () => {
+        await db.disconnect();
+        await redis.disconnect();
+        await rabbitmq.disconnect();
+        logger.info('Process terminated');
+        process.exit(0);
+      });
+    });
+
+    process.on('unhandledRejection', (err) => {
+      logger.error('Unhandled Rejection! Shutting down...', err);
+      server.close(() => {
+        process.exit(1);
+      });
     });
   } catch (error) {
-    console.error('Failed to start server:', error);
+    logger.error('Failed to start server:', error);
     process.exit(1);
   }
 };
