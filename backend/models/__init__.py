3ca7d229f7ce7ee7bdcc003c614b08f835ef16a1
Chamele-on-off <mot124@yandex.ru>
2025-06-01 01:45:10 +0300

    Create __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .transaction import Transaction
+from .triangle import TriangleTransaction
+from .requisites import Requisite
+from .dispute import Dispute
+from .audit_log import AuditLog
+
+__all__ = ['User', 'Transaction', 'TriangleTransaction', 'Requisite', 'Dispute', 'AuditLog']
