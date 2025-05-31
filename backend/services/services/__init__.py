b98c81f10511ceae833e582b2e80a29d28098885
Chamele-on-off <mot124@yandex.ru>
2025-06-01 01:42:57 +0300

    Create __init__.py

---
+++ backend/services/services/__init__.py
@@ -0,0 +1,5 @@
+from .transaction_service import TransactionService
+from .fraud_detection import FraudDetectionService
+from .notification_service import NotificationService
+
+__all__ = ['TransactionService', 'FraudDetectionService', 'NotificationService']
