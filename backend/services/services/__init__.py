155d996f7ee2ca587eaaa4475451516a18acc163
Chamele-on-off <mot124@yandex.ru>
2025-06-01 01:43:16 +0300

    Delete backend/services/services directory

--- backend/services/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .transaction_service import TransactionService
-from .fraud_detection import FraudDetectionService
-from .notification_service import NotificationService
-
-__all__ = ['TransactionService', 'FraudDetectionService', 'NotificationService']
